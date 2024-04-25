a0c407bf8e9c9990a15dc53e4a6e0cc2bc341500
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-25 18:57:01 -0300

    Add homepage styles

---
+++ src/styles/home.ts
@@ -0,0 +1,13 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const MainContainer = styled.main`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    min-height: calc(100vh - 136px);
+
+    padding: 24px 0;
+`
