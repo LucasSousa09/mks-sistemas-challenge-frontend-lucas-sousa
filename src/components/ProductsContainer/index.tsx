c2739954efcdb56ef713846936748160131b3c69
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-26 18:50:30 -0300

    Update ProductsContainer to fetch products data

--- src/components/ProductsContainer/index.tsx
+++ src/components/ProductsContainer/index.tsx
@@ -1,17 +1,37 @@
 import { Product } from "../Product"
 import { Container } from "./styles"
 
-export function ProductsContainer(){
+export type Product = {
+  id: number,
+  name: string,
+  description: string,
+  photo: string,
+  price: string,
+}
+
+export async function ProductsContainer(){
+    const res = await fetch('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?'  + new URLSearchParams({
+        page: "1",
+        rows: "8",
+        sortBy: "id",
+        orderBy: "DESC"
+      }))
+    
+    const { products }: {products: Product[]} = await res.json()
+
     return (
         <Container>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-
+            {
+                products.map((product) => (
+                <Product 
+                    id={product.id}
+                    key={product.id}
+                    name={product.name}
+                    photo={product.photo}
+                    price={product.price}
+                    description={product.description}
+                />))
+            }
         </Container>
     )
 }
