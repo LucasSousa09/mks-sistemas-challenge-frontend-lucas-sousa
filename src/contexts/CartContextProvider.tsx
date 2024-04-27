8399e763e59629fc1fdfb10d50ecded34fd3ad50
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-27 13:47:33 -0300

    Add localstorage to persist the cart

--- src/contexts/CartContextProvider.tsx
+++ src/contexts/CartContextProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
 type CartContextProps = {
     cart: CartProduct[],
@@ -32,31 +32,48 @@ export function CartContextProvider({children}: CartContextProviderProps){
     const [cart, setCart] = useState<CartProduct[]>([])
     const [isCartOpen, setIsCartOpen] = useState(false)
 
+    useEffect(() => {
+        const localStorageCart = localStorage.getItem('@mks-systems:cart')
+
+        if(localStorageCart !== null){
+            if(localStorageCart !== '[]'){
+                setCart(JSON.parse(localStorageCart))
+            }
+        }
+    },[])
+
+
     const updateCart = {
         addNewProductToCart(product: CartProduct){
             setCart(state => {
                 const productAlreadyOnCart = state.find(cartItem => product.id === cartItem.id)
 
-               if(productAlreadyOnCart === undefined){
-                   return [
+                if(productAlreadyOnCart === undefined){
+                    localStorage.setItem('@mks-systems:cart', JSON.stringify([...state, {...product, quantity: 1}]))
+                    return [
                     ...state, {
                         ...product,
                         quantity: 1
-                    }
+                        }
                     ]
-               }
-               return state
+                }
+                return state
             })
         },
 
         removeProductFromCart(productId: number){
-            setCart(state => state.filter(cartItem => cartItem.id !== productId))
+            setCart(state => {
+                const filteredArray = state.filter(cartItem => cartItem.id !== productId)
+                localStorage.setItem('@mks-systems:cart', JSON.stringify(filteredArray))
+                return filteredArray
+            })
         },
 
         addOneToCart(productId: number){
             setCart(state => {
                 return state.map(cartItem => {
                     if(cartItem.id === productId && cartItem.quantity){
+                        localStorage.setItem('@mks-systems:cart', JSON.stringify({ ...cartItem,quantity: cartItem.quantity + 1}))
                         return {
                             ...cartItem,
                             quantity: cartItem.quantity + 1
@@ -71,6 +88,7 @@ export function CartContextProvider({children}: CartContextProviderProps){
             setCart(state => {
                 return state.map(cartItem => {
                     if(cartItem.id === productId && cartItem.quantity && cartItem.quantity > 1){
+                        localStorage.setItem('@mks-systems:cart', JSON.stringify({ ...cartItem,quantity: cartItem.quantity + 1}))
                         return {
                             ...cartItem,
                             quantity: cartItem.quantity - 1
